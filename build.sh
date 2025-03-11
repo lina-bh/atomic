34505535ea61731f40e89fa9a3d1ee7485fbc529
lina-bh <36717206+lina-bh@users.noreply.github.com>
2025-03-11 17:57:25 +0000

    add steam 8)

--- build.sh
+++ build.sh
@@ -6,18 +6,36 @@ for copr in $coprs; do
   dnf5 -y copr enable "$copr"
 done
 dnf5 -y config-manager addrepo --overwrite --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
-# dnf5 -y install --nogpgcheck --repofrompath "terra,https://repos.fyralabs.com/terra\$releasever" terra-release{,-extras}
+dnf5 -y install \
+        "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+        "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 
 dnf5 -y install \
-  ublue-os-luks \
+  ublue-os-media-automount-udev \
   ublue-os-udev-rules \
   tailscale \
+  rsms-inter-fonts \
+  scx-scheds \
+  fish \
+  gamescope \
+  mangohud \
+  wl-clipboard \
+  ;
+dnf5 -y --setopt=install_weak_deps=False install \
+  steam \
+  ;
+dnf5 -y remove \
+  firefox \
+  firefox-langpacks \
   ;
 
+dnf5 -y config-manager setopt "*rpmfusion*".enabled=0
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/tailscale.repo
 for copr in $coprs; do
   dnf5 -y copr disable "$copr"
 done
+
+echo 'add_dracutmodules+=" fido2  "' > /usr/lib/dracut/dracut.conf.d/89-fido2.conf
 
 CSFG=/usr/lib/systemd/system-generators/coreos-sulogin-force-generator
 curl -sSLo ${CSFG} https://raw.githubusercontent.com/coreos/fedora-coreos-config/refs/heads/stable/overlay.d/05core/usr/lib/systemd/system-generators/coreos-sulogin-force-generator
